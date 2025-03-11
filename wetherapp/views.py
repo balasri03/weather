23f464b716e144efd4e889ca39414b1636ccf9b1
balasri03 <n210342@rguktn.ac.in>
2025-03-11 21:48:52 +0530

    Update views.py

--- wetherapp/views.py
+++ wetherapp/views.py
@@ -1,55 +1,4 @@
-# from django.shortcuts import render
-# import requests
-# import datetime
-# from django.contrib import messages
-# def home(request):
-#     if 'city' in request.POST:
-#         city = request.POST['city']
-#     else:
-#         city = 'Indore'
-#     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid='
-#     PARAMS = {'units':'metric'}
 
-#     API_KEY =  'AIzaSyCWISmywjFRw4HkxujVp49XIm35jQR2VAg'
-
-#     SEARCH_ENGINE_ID = '739abe9ea725c40ee'
-     
-#     query = city + " 1920x1080"
-#     page = 1
-#     start = (page - 1) * 10 + 1
-#     searchType = 'image'
-#     city_url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}&searchType={searchType}&imgSize=xlarge"
-
-#     data = requests.get(city_url).json()
-#     count = 1
-#     search_items = data.get("items")
-#     image_url = search_items[1]['link']
-#     response = requests.get(url, params=PARAMS)
-#     if response==200:
-#         data = response.json()
-#         description = data['weather'][0]['description']
-#         icon = data['weather'][0]['icon']
-#         temp = data['main']['temp']
-#         day = datetime.date.today()
-#         return render(request, 'index.html', {
-#             'description': description,
-#             'icon': icon,
-#             'temp': temp,
-#             'day': day,
-#             'city': city,
-#         })
-#     except:
-#         exception_occured=True
-#         messages.error(request,'entered city is not available to api')
-#         day=datetime.date.today()
-#         return render(request, 'index.html', {
-#             'description': 'clear sky',
-#             'icon': '01d',
-#             'temp': 25,
-#             'day': day,
-#             'city': 'indore',
-#             'exception_occured':True
-#         })
 
 from django.shortcuts import render
 import requests
@@ -57,7 +6,7 @@ import datetime
 from django.contrib import messages
 
 # Replace 'your_api_key' with your actual OpenWeatherMap API key
-API_KEY = '86deac85fe9e27625f6614cfe1c468c5'
+API_KEY = ''
 
 def home(request):
     city = request.POST.get('city', 'Indore')  # Get city from POST, default to 'Indore'
